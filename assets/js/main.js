38c6781fa55e9a915236fd906caf5c2af2dbbcc9
VladiSVS <uladzimir.siauko@gmail.com>
2021-08-19 00:06:45 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,16 @@ function einkommenSteuer() {
         rechnen()
     }
 
+    let selectedValue = document.getElementById("yearList")
+    let checkedYear = selectedValue.options[selectedValue.selectedIndex].value
+    if (checkedYear == "2021") {
+        rechnen()
+    } else if (checkedYear == "2020") {
+        rechnenOne()
+    } else if (checkedYear == "2019") {
+        rechnenTwo()
+    }
+
     function rechnen() {
         if (zvE <= 9744) {
             kirkSt.innerHTML = kirk
@@ -38,6 +48,63 @@ function einkommenSteuer() {
             splitting()
         } else if (zvE >= 274613) {
             esT = 0.45 * zvE - 17374.99
+            esT = Math.round(esT)
+            kirkRechner()
+            splitting()
+        }
+    }
+
+    function rechnenOne() {
+        if (zvE <= 9408) {
+            kirkSt.innerHTML = kirk
+            einResult.innerHTML = esT
+            gesamtB.innerHTML = esT
+            esT = Math.round(esT)
+        } else if (zvE >= 9409 && zvE <= 14532) {
+            y = (zvE - 9408) / 10000
+            esT = (972.87 * y + 1400) * y
+            esT = Math.round(esT)
+        } else if (zvE >= 14533 && zvE <= 57051) {
+            z = (zvE - 14532) / 10000
+            esT = (212.02 * z + 2397) * z + 972.79
+            esT = Math.round(esT)
+            kirkRechner()
+            splitting()
+        } else if (zvE >= 57052 && zvE <= 270500) {
+            esT = 0.42 * zvE - 8963.74
+            esT = Math.round(esT)
+            kirkRechner()
+            splitting()
+        } else if (zvE >= 270501) {
+            esT = 0.45 * zvE - 17078.74
+            esT = Math.round(esT)
+            kirkRechner()
+            splitting()
+        }
+    }
+
+    function rechnenTwo() {
+        if (zvE <= 9168) {
+            kirkSt.innerHTML = kirk
+            einResult.innerHTML = esT
+            gesamtB.innerHTML = esT
+        } else if (zvE >= 9169 && zvE <= 14254) {
+            y = (zvE - 9168) / 10000
+            esT = (980.14 * y + 1400) * y
+            esT = Math.round(esT)
+        } else if (zvE >= 14255 && zvE <= 55960) {
+            z = (zvE - 14254) / 10000
+            esT = (216.16 * z + 2397) * z + 965.58
+            esT = Math.round(esT)
+            kirkRechner()
+            splitting()
+        } else if (zvE >= 55961 && zvE <= 265326) {
+            esT = 0.42 * zvE - 8780.90
+            esT = Math.round(esT)
+            kirkRechner()
+            splitting()
+        } else if (zvE >= 265327) {
+            esT = 0.45 * zvE - 16740.68
             esT = Math.round(esT)
             kirkRechner()
             splitting()
@@ -85,3 +152,6 @@ function change() {
         betragTwo.style.display = "none"
     }
 }
+
+
+
